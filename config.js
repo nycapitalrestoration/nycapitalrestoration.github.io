002a04fb4da01e36d697666c17fb21adfcf89b3c
nycapitalrestoration <boiroi2309@gmail.com>
2025-11-29 18:11:48 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-config = {
-  "uri": "https://nycapitalrestoration.github.io",
-  "redirect_uri": "https://nycapitalrestoration.github.io/login.html",
-  "client_id": "058478e81bf4404f970d861bcd7fa8c0",
-  "slowdown_import": 200,
-  "slowdown_export": 200
+// config.js
+export const config = {
+  client_id: "YOUR_CLIENT_ID",
+  redirect_uri: "https://nycapitalrestoration.github.io/login.html",
+  slowdown_import: 200,
+  slowdown_export: 200
 };
