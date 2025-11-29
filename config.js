0ddcabdcbe7142ff1ea767692a489a60d5d459ba
nycapitalrestoration <boiroi2309@gmail.com>
2025-11-29 18:15:28 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 // config.js
+// Exported as an ES module
 export const config = {
-  client_id: "YOUR_CLIENT_ID",
-  redirect_uri: "https://nycapitalrestoration.github.io/login.html",
-  slowdown_import: 200,
-  slowdown_export: 200
+    client_id: "058478e81bf4404f970d861bcd7fa8c0",  // Your Spotify app client ID
+    redirect_uri: "https://nycapitalrestoration.github.io/login.html",
+    slowdown_import: 200,   // delay in ms for API calls
+    slowdown_export: 200
 };
